e4e67a17fe0d3d14804dbd482f92a3c4668eaeb2
akrj <abhishek@lollypop.design>
2019-10-23 00:07:48 +0530

    Updates

---
+++ precache-manifest.33e17a4d05f69aff032d21235973f1bf.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1d5c89088d825a36a72d2717ed7317b",
+    "url": "/anna/index.html"
+  },
+  {
+    "revision": "1d103b4acdf4c2af4c0c",
+    "url": "/anna/static/css/main.d37c3786.chunk.css"
+  },
+  {
+    "revision": "90b1cc27b766c9e279f5",
+    "url": "/anna/static/js/2.c6bb67a7.chunk.js"
+  },
+  {
+    "revision": "1d103b4acdf4c2af4c0c",
+    "url": "/anna/static/js/main.c0110710.chunk.js"
+  },
+  {
+    "revision": "e934141e251e22cd787d",
+    "url": "/anna/static/js/runtime-main.529b990d.js"
+  },
+  {
+    "revision": "93a7083985dcc05f7946a6b442bce9e9",
+    "url": "/anna/static/media/icon-arrow-left.93a70839.svg"
+  },
+  {
+    "revision": "ac190530af80c47bf457c5c7b61c7543",
+    "url": "/anna/static/media/icon-arrow-right.ac190530.svg"
+  },
+  {
+    "revision": "edb85ea641c49620c685e129468ce54c",
+    "url": "/anna/static/media/icon-facebook.edb85ea6.svg"
+  },
+  {
+    "revision": "4d49ddf60b164bc67ee23220acbf78cc",
+    "url": "/anna/static/media/icon-insta.4d49ddf6.svg"
+  },
+  {
+    "revision": "798c664c53cc1a50eab5565f4d4a1161",
+    "url": "/anna/static/media/icon-review.798c664c.svg"
+  },
+  {
+    "revision": "593cc15276668957d7266a540e3b654f",
+    "url": "/anna/static/media/icon-search.593cc152.svg"
+  },
+  {
+    "revision": "584b607f5c2ff075429dc0e7b8d142ef",
+    "url": "/anna/static/media/loader.584b607f.gif"
+  },
+  {
+    "revision": "0932ae79afa00ed158e680e233a92060",
+    "url": "/anna/static/media/logo.0932ae79.svg"
+  }
+]);
